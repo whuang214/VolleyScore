4665fb96620f8db9ec01b5ba610e7b3650e1f389
whuang214 <whuang2146@gmail.com>
2023-09-15 15:27:15 -0400

    reorganize file structure and add interface for match and team

---
+++ src/pages/App.tsx
@@ -0,0 +1,25 @@
+import { useState } from "react";
+import "../styles/App.css";
+
+interface Match {
+  teamOne: Team | null;
+  teamOneScore: number;
+  teamTwo: Team | null;
+  teamTwoScore: number;
+  winner: Team | null;
+}
+interface Team {
+  name: string;
+  sets: {
+    won: number;
+    lost: number;
+  };
+  matches: Match[];
+}
+
+export default function App() {
+  const [teamOne, setTeamOne] = useState<Team | null>(null);
+  const [teamTwo, setTeamTwo] = useState<Team | null>(null);
+
+  return <h1>Hello World</h1>;
+}
