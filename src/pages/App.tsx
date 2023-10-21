ceca56c55b6280b5ccf669b4521379a09ad8d13e
whuang214 <whuang2146@gmail.com>
2023-10-21 19:45:40 -0400

    more styles for the page

--- src/pages/App.tsx
+++ src/pages/App.tsx
@@ -1,5 +1,6 @@
 import { useState } from "react";
 import "../styles/App.css";
+import { Typography } from "@mui/material";
 
 interface Team {
   name: string;
@@ -16,9 +17,37 @@ export default function App() {
     score: 0,
   });
 
+  const incrementTeamOneScore = () => {
+    setTeamOne((prevState) => ({
+      ...prevState,
+      score: prevState.score + 1,
+    }));
+  };
+
+  const incrementTeamTwoScore = () => {
+    setTeamTwo((prevState) => ({
+      ...prevState,
+      score: prevState.score + 1,
+    }));
+  };
+
   return (
     <>
-      <h1>Hello World</h1>
+      <Typography variant="h1">Scoreboard</Typography>
+      <div className="button-container">
+        <div className="score-container">
+          <Typography variant="h4">{teamOne.name}</Typography>
+          <button className="score-button" onClick={incrementTeamOneScore}>
+            {teamOne.score}
+          </button>
+        </div>
+        <div className="score-container">
+          <Typography variant="h4">{teamTwo.name}</Typography>
+          <button className="score-button" onClick={incrementTeamTwoScore}>
+            {teamTwo.score}
+          </button>
+        </div>
+      </div>
     </>
   );
 }
